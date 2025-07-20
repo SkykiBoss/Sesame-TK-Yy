a36a471880dec56c2157de3f9062c39845bbc032
Youyu-Github <77781701+Youyu-Github@users.noreply.github.com>
2025-07-20 18:54:19 +0800

    py 加饭卡

--- app/src/main/java/fansirsqi/xposed/sesame/task/antFarm/AntFarm.java
+++ app/src/main/java/fansirsqi/xposed/sesame/task/antFarm/AntFarm.java
@@ -259,7 +259,8 @@ public class AntFarm extends ModelTask {
     private StringModelField giftFamilyDrawFragment;
     private BooleanModelField paradiseCoinExchangeBenefit;
     private SelectModelField paradiseCoinExchangeBenefitList;
-
+    private BooleanModelField useBigEaterTool;
+    
     @Override
     public ModelFields getFields() {
         ModelFields modelFields = new ModelFields();
@@ -309,6 +310,7 @@ public class AntFarm extends ModelTask {
         modelFields.addField(giftFamilyDrawFragment = new StringModelField("giftFamilyDrawFragment", "家庭 | 扭蛋碎片赠送用户ID(配置目录查看)", ""));
         modelFields.addField(paradiseCoinExchangeBenefit = new BooleanModelField("paradiseCoinExchangeBenefit", "小鸡乐园 | 兑换权益", false));
         modelFields.addField(paradiseCoinExchangeBenefitList = new SelectModelField("paradiseCoinExchangeBenefitList", "小鸡乐园 | 权益列表", new LinkedHashSet<>(), ParadiseCoinBenefit::getList));
+        modelFields.addField(useBigEaterTool = new BooleanModelField("useBigEaterTool", "加饭卡 | 使用", false));
         return modelFields;
     }
 
@@ -1515,6 +1517,13 @@ public class AntFarm extends ModelTask {
         try {
             if (foodStock < 180) {
                 Log.record(TAG, "喂鸡饲料不足");
+            } else if (useBigEaterTool.getValue() && foodStock >= 360 && useFarmTool(ownerFarmId, ToolType.BIG_EATER_TOOL)) {
+                JSONObject jo = syncAnimalStatus(ownerFarmId, "SYNC_USE_BIG_EATER_TOOL", "QUERY_EMOTION_INFO|QUERY_FARM_INFO|QUERY_USER_INFO");
+                if (jo != null) {
+                    parseSyncAnimalStatusResponse(jo);
+                }
+                Log.farm("使用加饭卡🥣投喂🐥成功#剩余饲料" + foodStock + "g");
+                return true;
             } else {
                 JSONObject jo = new JSONObject(AntFarmRpcCall.feedAnimal(farmId));
                 int feedFood = foodStock - jo.getInt("foodStock");
@@ -2678,9 +2687,9 @@ public class AntFarm extends ModelTask {
     }
 
     public enum ToolType {
-        STEALTOOL, ACCELERATETOOL, SHARETOOL, FENCETOOL, NEWEGGTOOL, DOLLTOOL, ORDINARY_ORNAMENT_TOOL, ADVANCE_ORNAMENT_TOOL;
+        STEALTOOL, ACCELERATETOOL, SHARETOOL, FENCETOOL, NEWEGGTOOL, DOLLTOOL, ORDINARY_ORNAMENT_TOOL, ADVANCE_ORNAMENT_TOOL, BIG_EATER_TOOL;
 
-        public static final CharSequence[] nickNames = {"蹭饭卡", "加速卡", "救济卡", "篱笆卡", "新蛋卡", "公仔补签卡", "普通装扮补签卡", "高级装扮补签卡"};
+        public static final CharSequence[] nickNames = {"蹭饭卡", "加速卡", "救济卡", "篱笆卡", "新蛋卡", "公仔补签卡", "普通装扮补签卡", "高级装扮补签卡", "加饭卡"};
 
         public CharSequence nickName() {
             return nickNames[ordinal()];
